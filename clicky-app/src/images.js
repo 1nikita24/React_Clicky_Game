66e5d54bdbb8d8c5784a193f8a43b33b879f000a
1nikita24 <nikitaezhov@icloud.com>
2019-08-27 19:55:58 -0400

    working

---
+++ clicky-app/src/images.js
@@ -0,0 +1,29 @@
+import adventuretime from './images/adventuretime.png';
+import ben10 from './images/ben10.png';
+import crabs from './images/crabs.png';
+import goofy from './images/goofy.png';
+import jhonny from './images/jhonny.png';
+import mickey from './images/mickey.png';
+import platapus from './images/platapus.png';
+import robbin from './images/robbin.png';
+import smurf from './images/smurf.png';
+import spongebob from './images/spongebob.png';
+import stewie from './images/stewie.png';
+import woodpicker from './images/woodpicker.png';
+
+const images = [
+  adventuretime,
+  ben10,
+  crabs,
+  goofy,
+  jhonny,
+  mickey,
+  platapus,
+  robbin,
+  smurf,
+  spongebob,
+  stewie,
+  woodpicker
+];
+
+export default images;
